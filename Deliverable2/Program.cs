2f0dd1526ad53ecc1b1ce22cb2b2f12def27484b
Reginald <youngreg21@gmail.com>
2020-01-06 02:36:17 -0500

    Commit for Deliverable 2

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            String message = "";
+            int checksum = 0;
+            char ch;
+            int num;
+            Console.WriteLine("What is your message?");
+            input = Console.ReadLine();
+            input = input.ToUpper();
+            for (int i = 0; i < input.Length; i++)
+            {
+                ch = input[i];
+                num = (int)ch;
+                checksum = checksum + num;
+                message = message + num.ToString() + "-";
+            }
+            message = message.Remove(message.Length - 1);
+            Console.WriteLine("Your encoded message is " + message);
+            Console.WriteLine("The message checksum is " + checksum);
+            
+
+        }
+    }
+}
